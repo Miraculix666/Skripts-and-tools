b77971a6549e42c9a240c1975ccfda8c1f15f7c1
Miraculix666 <57354802+Miraculix666@users.noreply.github.com>
2024-06-25 09:39:14 +0200

    Update SW_Installed.ps1

--- SW_Installed.ps1
+++ SW_Installed.ps1
@@ -2,16 +2,6 @@
 $computerName = "RemoteComputerName"
 $softwareName = "SoftwareName"
 
-# Test der Remote-Registry-Verbindung 
-try {
-    $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $computerName)
-    Write-Output "Die Remote-Verbindung zur Registry von $computerName wurde erfolgreich hergestellt."
-} catch {
-    Write-Output "Die Remote-Verbindung zur Registry von $computerName konnte nicht hergestellt werden. Fehler: $_"
-}
-Write-Output "Die Software $softwareName ist auf $computerName nicht installiert."
-}
-
 # Erstellen Sie eine Remote-Registry-Verbindung
 $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $computerName)
 
@@ -34,30 +24,3 @@ $regKey.GetSubKeyNames() | ForEach-Object {
 if (-not $softwareFound) {
     Write-Output "Es wurde keine Software gefunden, die den String $softwareName auf $computerName enthält."
 }
-
-
-
-##### Abfrage mit WMI
-
-# Versuchen Sie, eine Remote-Registry-Verbindung herzustellen
-try {
-    $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $computerName)
-    Write-Output "Die Remote-Verbindung zur Registry von $computerName wurde erfolgreich hergestellt."
-} catch {
-    Write-Output "Die Remote-Verbindung zur Registry von $computerName konnte nicht hergestellt werden. Fehler: $_"
-}
-
-
-# Erstellen Sie eine WMI-Abfrage
-$query = "SELECT * FROM Win32_Product WHERE (Name LIKE '%$softwareName%')"
-
-# Führen Sie die Abfrage auf dem Remote-Computer aus
-$software = Get-WmiObject -Query $query -ComputerName $computerName
-
-# Überprüfen Sie, ob die Software gefunden wurde
-if ($software) {
-    Write-Output "Die Software $softwareName ist auf $computerName installiert."
-} else {
-    Write-Output "Die Software $softwareName ist auf $computerName nicht installiert."
-}
-
