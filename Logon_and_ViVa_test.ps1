32aa329a8db811285938898abb3c3ee72748228c
Miraculix666 <57354802+Miraculix666@users.noreply.github.com>
2024-11-04 11:04:43 +0100

    Update Logon_and_ViVa_test.ps1

--- Logon_and_ViVa_test.ps1
+++ Logon_and_ViVa_test.ps1
@@ -1,6 +1,6 @@
-# Liste der PCs und Nutzer
-$pcs = "c26pbze70004600".."c26pbze70004613"
-$users = "L1101061".."L1101061"
+# Liste der PCs und Nutzer als Strings
+$pcs = @("c00", "c01", "c02", "c03", "c04", "c05", "c06", "c07", "c08", "c09", "c10", "c11", "c12", "c13")
+$users = @("N00", "N01", "N02", "N03", "N04", "N05", "N06", "N07", "N08", "N09", "N10", "N11", "N12", "N13")
 $password = "deinPasswort"  # Passwort für alle Nutzer
 
 foreach ($i in 0..($pcs.Length - 1)) {
@@ -9,13 +9,13 @@ foreach ($i in 0..($pcs.Length - 1)) {
     $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
     $credential = New-Object System.Management.Automation.PSCredential ($user, $securePassword)
 
-    # Öffne RDP-Sitzung
+    # Öffne RDP-Sitzung mit dem aktuellen Benutzer
     Start-Process -FilePath "mstsc.exe" -ArgumentList "/v:$pc /prompt"
 
     # Warte kurz, um sicherzustellen, dass die Verbindung hergestellt ist
     Start-Sleep -Seconds 5
 
-    # Starte die Anwendung auf dem entfernten PC
+    # Starte die Anwendung auf dem entfernten PC im Kontext eines anderen Benutzers
     Invoke-Command -ComputerName $pc -Credential $credential -ScriptBlock {
         Start-Process -FilePath "C:\Programme\x\x.exe"
     }
