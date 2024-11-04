c26b1e74fe0e5a0348b5b7059bb9c8a328f5a0f0
Miraculix666 <57354802+Miraculix666@users.noreply.github.com>
2024-11-04 11:15:35 +0100

    Update Logon_and_ViVa_test.ps1

--- Logon_and_ViVa_test.ps1
+++ Logon_and_ViVa_test.ps1
@@ -1,12 +1,15 @@
 # Liste der PCs und Nutzer als Strings
 $pcs = @("c26pbze70004600", "c26pbze70004601", "c26pbze70004602", "c26pbze70004603", "c26pbze70004604", "c26pbze70004605", "c26pbze70004606", "c26pbze70004607", "c26pbze70004608", "c26pbze70004609", "c26pbze70004610", "c26pbze70004611", "c26pbze70004612", "c26pbze70004613")
 $users = @("L1101061", "L1101062", "L1101063", "L1101064", "L1101065", "L1101066", "L1101067", "L1101068", "L1101069", "L1101070", "L1101071", "L1101072") 
-$password = "PW"  # Passwort für alle Nutzer
+
+# Frage nach dem Passwort für RDP und Benutzer
+$rdpPassword = Read-Host -AsSecureString "Bitte gib das RDP-Passwort ein"
+$userPassword = Read-Host -AsSecureString "Bitte gib das Passwort für die anderen Benutzer ein"
 
 foreach ($i in 0..($pcs.Length - 1)) {
     $pc = $pcs[$i]
     $user = $users[$i]
-    $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+    $securePassword = $userPassword
     $credential = New-Object System.Management.Automation.PSCredential ($user, $securePassword)
 
     # Öffne RDP-Sitzung mit dem aktuellen Benutzer
@@ -17,6 +20,6 @@ foreach ($i in 0..($pcs.Length - 1)) {
 
     # Starte die Anwendung auf dem entfernten PC im Kontext eines anderen Benutzers
     Invoke-Command -ComputerName $pc -Credential $credential -ScriptBlock {
-        Start-Process -FilePath "C:\Programme\x\x.exe"
+        Start-Process -FilePath "C:\Programme (x86)\viva\viva.exe"
     }
 }
