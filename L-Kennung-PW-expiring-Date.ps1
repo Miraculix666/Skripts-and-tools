51162854ea1171945ecb5b67829923761441444e
Miraculix666 <57354802+Miraculix666@users.noreply.github.com>
2025-04-02 11:57:46 +0200

    Update L-Kennung-PW-expiring-Date.ps1

--- L-Kennung-PW-expiring-Date.ps1
+++ L-Kennung-PW-expiring-Date.ps1
@@ -1,16 +1,13 @@
-
-
 # Active Directory-Modul importieren
 Import-Module ActiveDirectory
 
 # Aktuelle Domäne ermitteln
 $aktuelleDomäne = Get-ADDomain
+$domainName = $aktuelleDomäne.DNSRoot
 
-Write-Host "Aktuelle Domäne: $($aktuelleDomäne.DNSRoot)"
-
-# Definieren der OUs
-$ou81 = "OU=81,DC=IhrDomain,DC=com"
-$ou82 = "OU=82,DC=IhrDomain,DC=com"
+# OUs definieren
+$ou81 = "OU=81,$domainName"
+$ou82 = "OU=82,$domainName"
 
 # Ablaufdaten der Passwörter abrufen und exportieren
 Get-ADUser -Filter * -SearchBase $ou81 -Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" |
@@ -20,3 +17,5 @@ Export-Csv -Path "C:\temp\OU81_PasswordExpiration.csv" -NoTypeInformation
 Get-ADUser -Filter * -SearchBase $ou82 -Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" |
 Select-Object -Property "DisplayName", @{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} |
 Export-Csv -Path "C:\temp\OU82_PasswordExpiration.csv" -NoTypeInformation
+
+Write-Host "Daten erfolgreich exportiert."
