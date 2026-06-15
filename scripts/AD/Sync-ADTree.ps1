8f5f5d7ad4f2f5787ccb509a5e5399e4d29a4017
Miraculix <Miraculix666@gmail.com>
2026-06-15 15:38:09 +0200

    fix(ad): clean up copy-pasted parser error from top of Sync-ADTree.ps1

--- scripts/AD/Sync-ADTree.ps1
+++ scripts/AD/Sync-ADTree.ps1
@@ -1,17 +1,4 @@
 
-PS C:\Daten\Scripte\wip> C:\Daten\Scripte\AD-Sync-tree.ps1
-In C:\Daten\Scripte\AD-Sync-tree.ps1:27 Zeichen:81
-+ ... er beim Abrufen der Domänencontroller für Domäne $DomainName: $errorM ...
-+                                                      ~~~~~~~~~~~~
-Ungültiger Variablenverweis. Nach ":" folgte kein Zeichen, das für einen Variablennamen gültig ist. Verwenden Sie ggf. "${}", 
-um den Namen zu begrenzen.
-    + CategoryInfo          : ParserError: (:) [], ParentContainsErrorRecordException
-    + FullyQualifiedErrorId : InvalidVariableReferenceWithDrive
- 
-
-
-
-
 Import-Module ActiveDirectory
 
 try {
