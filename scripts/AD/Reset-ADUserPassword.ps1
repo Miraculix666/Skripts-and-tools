e6b187c300b36fa76e630bac5d7e017f6045d63b
Miraculix <Miraculix666@gmail.com>
2026-06-15 13:45:31 +0200

    feat(ad): import initial prepared version of Reset-ADUserPassword

---
+++ scripts/AD/Reset-ADUserPassword.ps1
@@ -0,0 +1,9 @@
+# Pre-filled options
+$preFilledUserName = ""
+$preFilledWildcard = "DWR45*"
+$preFilledOU = "OU=47678"
+$preFilledPassword = "P2f7aL4!01"
+$skipConfirmation = $false
+
+# Run the primary script with pre-filled options
+.\PrimaryScript.ps1 -UserName $preFilledUserName -Wildcard $preFilledWildcard -OU $preFilledOU -Password $preFilledPassword -SkipConfirmation:$skipConfirmation
